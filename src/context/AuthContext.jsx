ef0f9c786f80fbff7278de0a80eb6e995d485dc3
KiritiAryal <91573969+KiritiAryal@users.noreply.github.com>
2023-11-06 23:07:40 -0600

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,6 @@ export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState();
-
   useEffect(() => {
     userObserver(setCurrentUser);
   }, []);
